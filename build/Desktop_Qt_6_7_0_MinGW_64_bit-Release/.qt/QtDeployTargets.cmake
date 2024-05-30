fcae10abd316a25286a491b158fea1346ca424a8
Tacher3000 <puankovnikita386@gmail.com>
2024-05-30 23:26:13 +0300

    1 commit

--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_nAryTree_FILE C:/Users/pyanc/Desktop/qt/nAryTree/nAryTree/build/Desktop_Qt_6_7_0_MinGW_64_bit-Release/nAryTree.exe)
-set(__QT_DEPLOY_TARGET_nAryTree_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
